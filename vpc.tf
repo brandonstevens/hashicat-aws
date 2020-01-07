ad922c1527fbdce8340d5d7b97a1e2aa85c07c9e
Brandon Stevens <stevens.brandon@gmail.com>
2020-01-07 22:37:06 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-2a", "ap-south-2b"]
+  azs             = ["ap-south-1a", "ap-south-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
