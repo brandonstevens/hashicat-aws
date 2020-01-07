a9960cf45975ba46263dbf359e84001bc9e130cf
Brandon Stevens <bstevens@nextgen.com>
2020-01-07 23:40:29 +0000

    Restrict Ports

--- main.tf
+++ main.tf
@@ -30,28 +30,28 @@ resource aws_security_group "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["64.164.57.170/32"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["64.164.57.170/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["64.164.57.170/32"]
   }
 
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    cidr_blocks     = ["64.164.57.170/32"]
     prefix_list_ids = []
   }
 
@@ -77,7 +77,7 @@ resource aws_route_table "hashicat" {
   vpc_id = aws_vpc.hashicat.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "64.164.57.170/32"
     gateway_id = aws_internet_gateway.hashicat.id
   }
 }
